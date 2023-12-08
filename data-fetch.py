1c10a598303118a760c36116944ac4b97b16f1b9
Shamah M Zoha <shamah.zoha@gmail.com>
2023-12-08 16:01:41 +0600

    Add script for data download

---
+++ data-fetch.py
@@ -0,0 +1,13 @@
+import os
+import kaggle
+
+if not os.path.exists('./data'):
+    os.mkdir('./data/')
+
+print('Authenticating...')
+kaggle.api.authenticate()
+print('Authentication successful!')
+
+print('Downloading Dataset')
+kaggle.api.dataset_download_files('sulianova/cardiovascular-disease-dataset', './data', quiet=False, unzip=True)
+print('Dataset downloaded!');
