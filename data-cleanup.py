c1707afc29c9c4685f28e85c48d6d610fc8db782
Shamah M Zoha <shamah.zoha@gmail.com>
2023-12-08 19:50:55 +0600

    Add code for normalizing features and write to file

--- data-cleanup.py
+++ data-cleanup.py
@@ -1,3 +1,5 @@
+import os
+
 import pandas as pd
 from mrmr import mrmr_classif
 
@@ -13,3 +15,19 @@ print('=======================')
 print('Running mRMR for finding top 5 features')
 selected_features = mrmr_classif(X=features, y=target, K=5)
 print('Features selected by mRMR:', selected_features)
+
+features = features[selected_features]
+
+print('First 10 Rows of Features after selection:', features.head(20), sep='\n')
+print('=======================')
+
+features = (features - features.mean()) / features.std()
+print('First 10 Rows of Features after standardization:', features.head(20), sep='\n')
+
+if not os.path.exists('./data'):
+    os.makedirs('./data')
+
+print('Writing normalized dataset to ./data/cardio_train_updated.csv')
+output_dataframe = pd.concat([features, target], axis=1)
+output_dataframe.to_csv('./data/cardio_train_updated.csv', index=False)
+print('Write successful!')
