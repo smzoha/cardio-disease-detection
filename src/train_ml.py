f9591a55fd68bd08889bcaca49d1c49f4a3958b0
Shamah M Zoha <shamah.zoha@gmail.com>
2023-12-09 19:29:24 +0600

    Explained GNB & SVM using LIME

--- src/train_ml.py
+++ src/train_ml.py
@@ -1,4 +1,7 @@
+import numpy as np
 import pandas as pd
+from lime.lime_tabular import LimeTabularExplainer
+from matplotlib import pyplot as plt
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import LinearSVC
@@ -21,3 +24,14 @@ print('====== SVM ======')
 svm = LinearSVC(dual=False)
 svm_cv = CalibratedClassifierCV(svm)
 train_valid_test(svm_cv, 'SVM', x_train, y_train, x_test, y_test)
+
+print('====== Explaining SVM ======')
+explainer = LimeTabularExplainer(np.array(x_train), feature_names=x_train.columns, verbose=True)
+
+print('First instance of test data:', x_test.iloc[0], sep='\n')
+print('Corresponding true value:', y_test.iloc[0])
+print('================')
+
+explain_nb = explainer.explain_instance(x_test.iloc[0], svm_cv.predict_proba)
+explain_nb.as_pyplot_figure()
+plt.show()
